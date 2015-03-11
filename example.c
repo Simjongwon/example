dff17c357b37f485a7f1ccd66882168c7f3338b7
SeunghoHong <seunghonice@gmail.com>
2015-03-11 17:46:47 +0900

    master

---
+++ example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int addtion(int a, int b)
+{
+	//addition
+}
+
+int subtraction(int a, int b)
+{
+	//subtraction
+}
+
+int multiplication(int a, int b)
+{
+	//multiplication
+}
